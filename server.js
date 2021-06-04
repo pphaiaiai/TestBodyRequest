283a4896931cadfd870c1797ccaf7e06f95b28cb
First <natthasit.pan@innovasive.co.th>
2021-06-04 17:54:57 +0700

    add log res.body

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ app.use(bodyParser.json())
 
 app.post('/testGetBody', async (req, res) => {
   const reqBody = req.body
-
+  
   try {
+    console.log("🚀 ~ file: server.js ~ line 10 ~ app.post ~ reqBody", reqBody)
     res.status(202).json(reqBody)
   }catch (e) {
     console.log("🚀 ~ file: main.js ~ line 122 ~ app.post ~ e", e)
