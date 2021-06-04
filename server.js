f75dc2ced4dd4a590c00b8e9d055ba3980a4b9eb
First <natthasit.pan@innovasive.co.th>
2021-06-04 17:45:06 +0700

    init project

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.post('/testGetBody', async (req, res) => {
+  const reqBody = req.body
+
+  try {
+    res.status(202).json(reqBody)
+  }catch (e) {
+    console.log("🚀 ~ file: main.js ~ line 122 ~ app.post ~ e", e)
+    res.status(400).send('Bad Request')
+  }
+})
+
+
+app.listen(7000, () => {
+  console.log('Start server at port 7000.')
+})
